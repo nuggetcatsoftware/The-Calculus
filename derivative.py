e80564d3be49290594dcb1c5cdacbecd5fdafefb
nuggetcatsoftware <nuggetcatsoftware@gmail.com>
2021-09-04 20:09:12 +0800

    euler's number with factorial method

--- derivative.py
+++ derivative.py
@@ -1,4 +1,5 @@
 from math import *
+import math
 def f(x): #simple multiplication bruh
     return x ** 2
 def derive(function, value):
@@ -8,3 +9,20 @@ def derive(function, value):
     slope = top / bottom
     # Returns the slope to the third decimal
     return float("%.3f" % slope)
+
+def euler(n):
+    t_sum = 0
+    for i in range(n):
+        term = 1/math.factorial(i)
+        t_sum = t_sum + term
+    
+    return t_sum
+
+# Reading number of terms to be considered in series
+terms = int(input("Enter number of terms: "))
+
+# Function call
+e = euler(terms)
+
+# Displaying result
+print("e = ", e)
